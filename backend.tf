6751cec6179ffd1c62ccf8ea849b9cf6992c5a24
franck <franckwilly26@gmail.com>
2024-08-03 07:26:55 -0400

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "w7-fk-terraform" #replace with your bucket
+    bucket         = "hghgjhjhkjj" #replace with your bucket
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "locktable" #replace with the table
+    dynamodb_table = "hghgjhjhjklkjlklk" #replace with the table
   }
 }
